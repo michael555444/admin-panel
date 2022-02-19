17adfb6e4c807c0071dd4aa6ddeaec77348516be
Michael Kizimov <www.michael555444.by@gmail.com>
2022-02-19 10:18:20 +0300

    Release Itransition 3rd task deploy v8

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,3 +1,5 @@
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.views.generic import RedirectView
 from django.urls import path, include
 
 from mysite.core import views
@@ -8,6 +10,7 @@ urlpatterns = [
     path('admin/', views.secret_page, name='secret'),
     # path('secret2/', views.SecretPage.as_view(), name='secret2'),
     path('accounts/', include('django.contrib.auth.urls')),
+    path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url('img/favicon.ico')))
     # path('admin/', admin.site.urls),
     # path('hello/', request_page)
 ]
